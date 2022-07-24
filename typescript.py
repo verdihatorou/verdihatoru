1d5bf8cb2378844655c5274ca9a899c32f110e83
Perkenalkan nama saya <95448883+verdihatorou@users.noreply.github.com>
2022-07-25 01:21:13 +0700

    Create typescript.py

---
+++ typescript.py
@@ -0,0 +1,12 @@
+}{
+'Typescript'
+  code/3g4528819884584978
+  code/649813751997857990
+  random.code/play.code
+  <Golang>     
+ Golang. auto crash    <<>>  => crash.py
+'Heck clint'
+  clint.py
+   hide.py
+   start.code/
+}{
